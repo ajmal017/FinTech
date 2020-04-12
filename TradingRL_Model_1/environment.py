d3b9b982174fd71b568cea628bfcb4d0db0e5c1b
alitootoonchian <a.tootoonchian@gmail.com>
2020-04-11 18:24:02 -0700

    First Model with RL, run on GCP, 1500 episodes

---
+++ TradingRL_Model_1/environment.py
@@ -0,0 +1,112 @@
+import gym
+from gym import spaces
+from gym.utils import seeding
+import numpy as np
+import itertools
+
+
+class TradingEnv(gym.Env):
+    """
+    A 3-stock (MSFT, IBM, QCOM) trading environment.
+    State: [# of stock owned, current stock prices, cash in hand]
+      - array of length n_stock * 2 + 1
+      - price is discretized (to integer) to reduce state space
+      - use close price for each stock
+      - cash in hand is evaluated at each step based on action performed
+    Action: sell (0), hold (1), and buy (2)
+      - when selling, sell all the shares
+      - when buying, buy as many as cash in hand allows
+      - if buying multiple stock, equally distribute cash in hand and then utilize the balance
+    """
+    def __init__(self, train_data, init_invest=20000):
+        # data
+        # round up to integer to reduce state space
+        self.stock_price_history = np.around(train_data)
+        print(self.stock_price_history.shape)
+        self.n_stock, self.n_step = self.stock_price_history.shape
+
+        # instance attributes
+        self.init_invest = init_invest
+        self.cur_step = None
+        self.stock_owned = None
+        self.stock_price = None
+        self.cash_in_hand = None
+        self.np_random = None
+
+        # action space
+        self.action_space = spaces.Discrete(3**self.n_stock)
+
+        # observation space: give estimates in order to sample and build scaler
+        stock_max_price = self.stock_price_history.max(axis=1)
+        stock_range = [init_invest * 2 // mx for mx in stock_max_price]
+        space_range = (np.concatenate((stock_range, stock_max_price, [init_invest * 2]), axis=0))
+        self.observation_space = spaces.MultiDiscrete(space_range)
+
+        # seed and start
+        self.seed()
+        self.reset()
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def reset(self):
+        self.cur_step = 0
+        self.stock_owned = [0] * self.n_stock
+        self.stock_price = self.stock_price_history[:, self.cur_step]
+        self.cash_in_hand = self.init_invest
+        return self.get_obs()
+
+    def step(self, action):
+        assert self.action_space.contains(action)
+        prev_val = self.get_val()
+        self.cur_step += 1
+        self.stock_price = self.stock_price_history[:, self.cur_step]
+        self.trade(action)
+        cur_val = self.get_val()
+        reward = cur_val - prev_val
+        done = self.cur_step == self.n_step - 1
+        info = {'cur_val': cur_val, 'cur_step': self.cur_step}
+        return self.get_obs(), reward, done, info
+
+    def get_obs(self):
+        obs = []
+        obs.extend(self.stock_owned)
+        obs.extend(list(self.stock_price))
+        obs.append(self.cash_in_hand)
+        return obs
+
+    def get_val(self):
+        return np.sum(self.stock_owned * self.stock_price) + self.cash_in_hand
+
+    def trade(self, action):
+        # all combo to sell(0), hold(1), or buy(2) stocks
+        action_vec = 0
+        for i, e in enumerate(itertools.product([0, 1, 2], repeat=self.n_stock)):
+            if i == action:
+                action_vec = e
+
+        # one pass to get sell/buy index
+        sell_index = []
+        buy_index = []
+        for i, a in enumerate(action_vec):
+            if a == 0:
+                sell_index.append(i)
+            elif a == 2:
+                buy_index.append(i)
+
+        # two passes: sell first, then buy; might be naive in real-world settings
+        if sell_index:
+            for i in sell_index:
+                self.cash_in_hand += self.stock_price[i] * self.stock_owned[i]
+                self.stock_owned[i] = 0
+        if buy_index:
+            can_buy = True
+            while can_buy:
+                for i in buy_index:
+                    if self.cash_in_hand > self.stock_price[i]:
+                        # buy one share
+                        self.stock_owned[i] += 1
+                        self.cash_in_hand -= self.stock_price[i]
+                    else:
+                        can_buy = False
---
+++ TradingRL_Model_1/util.py
@@ -0,0 +1,49 @@
+import os
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import datetime as dt
+import pandas_datareader.data  as pdr
+
+
+def get_data(stock_intervals=10, col='Adj Close'):
+    """
+    Returns a 3 x n_step array
+    """
+    end_date = dt.date(2019, 12, 29)
+    start_date = end_date - dt.timedelta(365 * stock_intervals)
+    msft = pdr.get_data_yahoo('MSFT', start_date, end_date)
+    ibm = pdr.get_data_yahoo('IBM', start_date, end_date)
+    qcom = pdr.get_data_yahoo('QCOM', start_date, end_date)
+
+    # recent price are at top; reverse it
+    return np.array([msft[col].values[::-1],
+                     ibm[col].values[::-1],
+                     qcom[col].values[::-1]])
+
+
+def get_scaler(env):
+    """
+    Takes a env and returns a scaler for its observation space
+    """
+    low = [0] * (env.n_stock * 2 + 1)
+
+    high = []
+    max_price = env.stock_price_history.max(axis=1)
+    min_price = env.stock_price_history.min(axis=1)
+    # 3 is a magic number...
+    max_cash = env.init_invest * 3
+    max_stock_owned = max_cash // min_price
+    for i in max_stock_owned:
+        high.append(i)
+    for i in max_price:
+        high.append(i)
+    high.append(max_cash)
+
+    scaler = StandardScaler()
+    scaler.fit([low, high])
+    return scaler
+
+
+def maybe_make_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
